d12c8ad0365ea1f6fe548205015df2f1604796c3
Wasal-Rasool <wasalrasool37@gmail.com>
2022-02-19 22:13:20 +0500

    random gradient to cards added

--- frontend/src/component/Project/ProjectCard.js
+++ frontend/src/component/Project/ProjectCard.js
@@ -3,9 +3,19 @@ import "./ProjectCard.css";
 import Background from "../Project/background.png";
 
 const ProjectCard = ({ project }) => {
+  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  randomColor = "#" + randomColor;
+  var randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
+  randomColor2 = "#" + randomColor2;
+  var randomColor3 = Math.floor(Math.random() * 16777215).toString(16);
+  randomColor3 = "#" + randomColor3;
   return (
     <div
-      style={{ backgroundImage: `url(${Background})`, objectFit: "contain" }}
+      style={{
+        backgroundImage: `url(${Background})`,
+        objectFit: "contain",
+        backgroundImage: `linear-gradient(to bottom right, ${randomColor}, ${randomColor2},${randomColor3})`,
+      }}
       className="project-card"
     >
       <h1>{project.name}</h1>
